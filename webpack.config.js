09fe396fe49034ff3183556142d409bb40c1dccc
Roshan Narayan <rnarayan7@users.noreply.github.com>
2018-05-17 14:21:20 -0700

    Model based off of ReduxCasts

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,31 @@
-const path = require('path');
-const SRC_DIR = path.join(__dirname, '/react-client/src');
-const DIST_DIR = path.join(__dirname, '/react-client/dist');
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
-  output: {
-    path: DIST_DIR,
-    filename: 'bundle.js',
-  },
-  module : {
-    rules : [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.png$/,
-        loader: 'url-loader?limit=100000&minetype=image/png'
-      },
-      {
-        test: /\.jpg/,
-        loader: 'file-loader'
-      },
-      {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-       }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'app.bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        },
+    ],
+},
+plugins: [
+    new webpack.NamedModulesPlugin(),
+]
 };
