36da28f1653302979eaf06c09fc2af6d6791ad74
Roshan Narayan <rnarayan7@users.noreply.github.com>
2018-09-21 11:00:33 -0400

    Revert to 825698a98cc90d23e410be5579dba86713b267bd

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-
-import App from "./components/app";
-import reducers from "./reducers";
-
-ReactDOM.render( <App />, document.querySelector(".container"));
